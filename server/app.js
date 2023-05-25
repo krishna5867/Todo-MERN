9c226b64086f1fc9f58417370db6dec42039cead
Krishna Kumar <106882782+krishna5867@users.noreply.github.com>
2023-05-25 10:07:56 +0530

    fix

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require('express')
 const cors = require("cors")
+const cookieParser = require("cookie-parser");
 const connectDB = require('./config/db')
 
 const studentRoute = require('./routes/studentRoute')
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser());
 
 connectDB()
 
