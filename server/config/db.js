1c6d2a0f56897299a95f654b840d95ad2232cc03
Krishna Kumar <106882782+krishna5867@users.noreply.github.com>
2023-05-25 10:39:35 +0530

    deploy

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = () => {
     mongoose
-    .connect(process.env.mongo_URI,{
+    .connect(process.env.MONGO_URI,{
         useNewUrlParser: true,
         useUnifiedTopology: true
 
